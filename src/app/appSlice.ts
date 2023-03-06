6d3ec5d96d18a1ef8483d743d0638e6a98907c88
zemosamurai <zemosamurai@gmail.com>
2023-03-07 02:48:07 +0300

    refactor: edit state (infoSnackbar)

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,25 +1,35 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+export type VariantsSnackbarType = 'info' | 'success' | 'error' | 'warning'
+
 type InitialStateType = {
   statusLoading: boolean
-  infoMessage: string | null
+  infoSnackbar: {
+    message: string | null
+    variant?: VariantsSnackbarType
+  }
+}
+
+type SetInfoType = {
+  message: string | null
+  variant?: VariantsSnackbarType
 }
 
 const appSlice = createSlice({
   name: 'app',
   initialState: {
     statusLoading: false,
-    infoMessage: null,
+    infoSnackbar: { message: null, variant: undefined },
   } as InitialStateType,
   reducers: {
     setStatusLoading: (state, action: PayloadAction<boolean>) => {
       state.statusLoading = action.payload
     },
-    setInfoMessage: (state, action: PayloadAction<string | null>) => {
-      state.infoMessage = action.payload
+    setInfoSnackbar: (state, action: PayloadAction<SetInfoType>) => {
+      state.infoSnackbar = action.payload
     },
   },
 })
 
-export const { setStatusLoading, setInfoMessage } = appSlice.actions
+export const { setStatusLoading, setInfoSnackbar } = appSlice.actions
 export const appReducer = appSlice.reducer
