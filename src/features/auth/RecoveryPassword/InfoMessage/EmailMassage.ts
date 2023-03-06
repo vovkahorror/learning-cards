71d70defb36b3df172cbc76d519a2884539075cb
zemosamurai <zemosamurai@gmail.com>
2023-03-06 22:19:50 +0300

    feat: message for Email

---
+++ src/features/auth/RecoveryPassword/InfoMessage/EmailMassage.ts
@@ -0,0 +1,32 @@
+export const emailMassage = `
+<div style="background-color: #fff; padding: 15px;">
+    <p style="font-size: 22px; 
+       font-weight: 600; 
+       margin: 0;
+       color: #000000;
+       text-align: center">
+            Forgot your password?
+    </p><br/>
+                    
+    <p style="color: #868686; 
+       font-size: 16px; 
+       text-align: center; 
+       margin: 0;
+       text-align: center;">
+            That's okay, it happens! Click on the button bellow to reset your password
+    </p><br/>
+                    
+    <a href='http://localhost:3000/#/set-new-password/$token$'
+       style=" display: block;
+               width: fit-content;
+               margin: 0 auto;
+               padding: 10px 50px; 
+               background-color: #366EFF;
+               text-decoration: none;
+               border-radius: 30px;
+               font-size: 16px;
+               color: #ffffff;
+               font-weight: 600;">
+       Reset Your Password
+    </a>
+</div>`
