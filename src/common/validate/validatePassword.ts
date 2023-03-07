7bbebcb473da5d36ac3ce700b1f7ed66c8ecd708
zemosamurai <zemosamurai@gmail.com>
2023-03-07 23:26:31 +0300

    feat: validation when registering and changing the password

---
+++ src/common/validate/validatePassword.ts
@@ -0,0 +1,41 @@
+export const validatePassword = (value: string): any => {
+  if (!value) {
+    return null
+  }
+
+  const isNonWhiteSpace = /^\S*$/
+
+  if (!isNonWhiteSpace.test(value)) {
+    return 'Password must not contain Whitespaces.'
+  }
+
+  const isContainsUppercase = /^(?=.*[A-Z]).*$/
+
+  if (!isContainsUppercase.test(value)) {
+    return 'Password must have at least one Uppercase Character.'
+  }
+
+  const isContainsLowercase = /^(?=.*[a-z]).*$/
+
+  if (!isContainsLowercase.test(value)) {
+    return 'Password must have at least one Lowercase Character.'
+  }
+
+  const isContainsNumber = /^(?=.*[0-9]).*$/
+
+  if (!isContainsNumber.test(value)) {
+    return 'Password must contain at least one Digit.'
+  }
+
+  const isContainsSymbol = /^(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*$/
+
+  if (!isContainsSymbol.test(value)) {
+    return 'Password must contain at least one Special Symbol.'
+  }
+
+  const isValidLength = /^.{8,}$/
+
+  if (!isValidLength.test(value)) {
+    return 'Minimum number of Characters 8.'
+  }
+}
