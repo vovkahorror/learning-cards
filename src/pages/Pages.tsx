68edb177ba90de3ab48208d7a4c78df43cbd35a1
zemosamurai <zemosamurai@gmail.com>
2023-03-08 19:24:55 +0300

    feat: use hoc from initialized

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import { Route, Routes, Navigate } from 'react-router-dom'
 
+import { RequireAuth } from 'common/hoc/RequireAuth'
 import { AuthWrapper } from 'features/auth/Auth/AuthWrapper'
 import { Login } from 'features/auth/Login/Login'
 import { InfoMessage } from 'features/auth/RecoveryPassword/InfoMessage/InfoMessage'
-import { NewPassword } from 'features/auth/RecoveryPassword/NewPassword'
-import { RecoveryPassword } from 'features/auth/RecoveryPassword/RecoveryPassword'
+import { NewPassword } from 'features/auth/RecoveryPassword/NewPassword/NewPassword'
+import { Recovery } from 'features/auth/RecoveryPassword/Recovery/Recovery'
 import { Registration } from 'features/auth/Registration/Registration'
 import { NotFound } from 'features/NotFound/NotFound'
 import { Profile } from 'features/Profile/Profile'
@@ -15,14 +16,16 @@ import { PATH } from 'pages/path'
 export const Pages = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
-
       <Route element={<AuthWrapper />}>
         <Route path={PATH.LOGIN} element={<Login />} />
         <Route path={PATH.REGISTRATION} element={<Registration />} />
-        <Route path={PATH.FORGOT_PASSWORD} element={<RecoveryPassword />} />
+        <Route path={PATH.FORGOT_PASSWORD} element={<Recovery />} />
         <Route path={PATH.CHECK_EMAIL} element={<InfoMessage />} />
         <Route path={PATH.SET_NEW_PASSWORD} element={<NewPassword />} />
+      </Route>
+
+      <Route element={<RequireAuth />}>
+        <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
         <Route path={PATH.PROFILE} element={<Profile />} />
       </Route>
 
