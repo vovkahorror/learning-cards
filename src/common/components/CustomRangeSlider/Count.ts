4664364bd0a664a740c3f756ac9db47defd3694a
zemosamurai <zemosamurai@gmail.com>
2023-03-12 21:10:48 +0300

    feat: add component custom range slider

---
+++ src/common/components/CustomRangeSlider/Count.ts
@@ -0,0 +1,12 @@
+import { Box } from 'common/components/Layout/Box'
+import { styled } from 'styles/theme'
+
+export const Count = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 36px;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+`
