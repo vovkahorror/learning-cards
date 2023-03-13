9632dcf5c5ca2d01521017711d9381fb5272fbc7
krawtschenko <eugenykravchenko@gmail.com>
2023-03-13 13:35:40 +0100

    feat: added icons for actions

--- src/features/packs/PackList/PackList.tsx
+++ src/features/packs/PackList/PackList.tsx
@@ -1,41 +1,56 @@
 import React from 'react'
 
+import { IoTrashOutline, IoPencil, IoSkull } from 'react-icons/io5'
 import { Table } from 'rsuite'
 
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
 import { CardPacksType } from 'features/packs/packsAPI'
+import { deletePackTC } from 'features/packs/packsSlice'
 
 const { Column, HeaderCell, Cell } = Table
 
 export const PackList = () => {
+  const dispatch = useAppDispatch()
   const data = useAppSelector<CardPacksType[]>(state => state.packs.cardPacks)
+  const userId = useAppSelector(state => state.auth.user._id)
+
+  const removePack = (id: string) => {
+    dispatch(deletePackTC(id))
+  }
 
   return (
     <div>
       <Table height={400} data={data}>
-        <Column width={150} align="center" fixed>
+        <Column width={200} align="center" fixed>
           <HeaderCell>Name</HeaderCell>
           <Cell dataKey="name" />
         </Column>
 
-        <Column width={150}>
+        <Column width={200}>
           <HeaderCell>Cards</HeaderCell>
           <Cell dataKey="cardsCount" />
         </Column>
 
-        <Column width={150}>
+        <Column width={200}>
           <HeaderCell>Last Updated</HeaderCell>
           <Cell dataKey="updated" />
         </Column>
 
-        <Column width={150}>
+        <Column width={200}>
           <HeaderCell>Created by</HeaderCell>
           <Cell dataKey="user_name" />
         </Column>
 
-        <Column width={100}>
+        <Column width={200}>
           <HeaderCell>Actions</HeaderCell>
-          <Cell dataKey="actions" />
+          <Cell>
+            {rowData =>
+              rowData.user_id !== userId && (
+                <IoTrashOutline onClick={() => removePack(rowData._id)} />
+              )
+            }
+          </Cell>
         </Column>
       </Table>
     </div>
