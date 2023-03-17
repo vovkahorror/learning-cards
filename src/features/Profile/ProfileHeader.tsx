25448ca86ef55ee810b963931e80601e5be391fc
zemosamurai <zemosamurai@gmail.com>
2023-03-17 17:13:36 +0300

    feat: add dropdown on click profile img header

--- src/features/Profile/ProfileHeader.tsx
+++ src/features/Profile/ProfileHeader.tsx
@@ -1,28 +1,57 @@
 import React from 'react'
 
 import { useNavigate } from 'react-router-dom'
+import { Dropdown, IconButton } from 'rsuite'
 
-import profileImg from '../../assets/img/profile.png'
-
+import { ReactComponent as LogOutIcon } from 'assets/img/logOutLogo.svg'
+import profileImg from 'assets/img/profile.png'
+import { ReactComponent as ProfileIcon } from 'assets/img/profileLogo.svg'
 import { Box } from 'common/components/Layout/Box'
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
-import { ProfileAvatarImg } from 'features/Profile/profile.styled'
+import { logoutTC } from 'features/auth/authSlice'
 import { PATH } from 'pages/path'
+
+const renderIconButton = (props: any, ref: any) => {
+  return (
+    <IconButton
+      {...props}
+      ref={ref}
+      icon={<img src={profileImg} alt="" style={{ width: '36px', height: '36px' }} />}
+      circle
+      style={{ width: '36px', height: '36px' }}
+      appearance="primary"
+    />
+  )
+}
 
 export const ProfileHeader = () => {
   const user = useAppSelector(state => state.auth.user)
+  const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
-  const navigateToProfile = () => {
-    navigate(PATH.PROFILE)
-  }
+  const handlerNavigateToProfile = () => navigate(PATH.PROFILE)
+  const handlerLogOut = () => dispatch(logoutTC())
 
   return (
     <Box display={'flex'} alignItems={'center'} style={{ position: 'relative' }}>
       <p style={{ color: 'white', marginRight: '10px' }}>{user.name}</p>
-      <ProfileAvatarImg onClick={navigateToProfile} style={{ width: '36px', height: '36px' }}>
-        <img src={profileImg} alt="" />
-      </ProfileAvatarImg>
+      <Dropdown placement="bottomEnd" renderToggle={renderIconButton}>
+        <Dropdown.Item
+          onClick={handlerNavigateToProfile}
+          icon={<ProfileIcon />}
+          style={{ display: 'flex', alignItems: 'center', columnGap: '10px' }}
+        >
+          Profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={handlerLogOut}
+          icon={<LogOutIcon />}
+          style={{ display: 'flex', alignItems: 'center', columnGap: '10px' }}
+        >
+          Logout
+        </Dropdown.Item>
+      </Dropdown>
     </Box>
   )
 }
