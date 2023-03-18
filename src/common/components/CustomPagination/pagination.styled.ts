03e8473abc4f4b1ed8b5dc31bdb7625c7f369bbc
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2023-03-18 22:57:43 +0300

    some fixes and add theme switcher

---
+++ src/common/components/CustomPagination/pagination.styled.ts
@@ -0,0 +1,9 @@
+import Pagination from 'rsuite/esm/Pagination'
+
+import { styled } from 'styles/theme'
+
+export const PaginationContainer = styled(Pagination)`
+  background-color: var(--rs-bg-card);
+  border-radius: 10px;
+  padding: 5px 10px;
+`
