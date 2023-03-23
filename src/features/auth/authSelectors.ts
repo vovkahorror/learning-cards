6754c840d70f702dc993cd91307d1b3919a99a12
zemosamurai <zemosamurai@gmail.com>
2023-03-23 22:40:29 +0300

    ref: small fix userEmail from InfoMessage

--- src/features/auth/authSelectors.ts
+++ src/features/auth/authSelectors.ts
@@ -7,3 +7,4 @@ export const isLoggedIn = (state: RootStateType) => state.auth.isLoggedIn
 export const isSetNewPassword = (state: RootStateType) => state.auth.isSetNewPassword
 export const isSetRecovery = (state: RootStateType) => state.auth.isSetRecovery
 export const registerSuccess = (state: RootStateType) => state.auth.registerSuccess
+export const userEmailForEmail = (state: RootStateType) => state.auth.userEmail
